6a5baaadb9bfde41eab450804f46df9e0da89f98
Jean-François Grang <jfgrang@2appaz.com>
2020-04-06 01:12:55 +0200

    - Fixed pricing display issues- Added data to analytics

--- Purchasely/Frameworks/Purchasely.xcframework/ios-armv7_arm64/Purchasely.framework/Headers/Purchasely-Swift.h
+++ Purchasely/Frameworks/Purchasely.xcframework/ios-armv7_arm64/Purchasely.framework/Headers/Purchasely-Swift.h
@@ -274,6 +274,8 @@ SWIFT_CLASS("_TtC10Purchasely15PLYSubscription")
 
 
 
+
+
 #if __has_attribute(external_source_symbol)
 # pragma clang attribute pop
 #endif
@@ -521,6 +523,8 @@ SWIFT_CLASS("_TtC10Purchasely15PLYSubscription")
 - (nonnull instancetype)init SWIFT_UNAVAILABLE;
 + (nonnull instancetype)new SWIFT_UNAVAILABLE_MSG("-init is unavailable");
 @end
+
+
 
 
 
